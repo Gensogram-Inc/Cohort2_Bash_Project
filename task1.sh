f30fcb21675ea1ee56edc5704138708ad925c534
mary <miwuezi@outlook.com>
2025-04-10 23:27:34 +0000

    update code in task1.sh

--- task1.sh
+++ task1.sh
@@ -1,42 +1,41 @@
 #!/bin/bash
 
 #note: script is not compatible with sh because [[...]] is not supported in sh. use ./filename.sh instead of sh filename.sh
-
-#Task: 1. Simple Calculator: Build a calculator that supports +,-,/ and *. The script should be executed this way: ./calc.sh 5 + 3
-
 echo " let's do the calculation for you! "
 calculator () {
-        if [ $# -ne 3 ]; then
-            echo "Ensure line arguument is equal to 3"
+        if [ "$#" -ne 3 ]; then
+            echo "line arguument exceeds 3"
             echo "Usage: $0 <number1> <operator> <number2>"
             exit 1
-	fi
-
+        fi
         #validate numbers
         if ! [[ "$1" =~ ^-?[0-9]+$ ]]; then
             echo "Error: First argument is not a number."
             exit 1
-	fi
-
-	#validate operator
+        fi
+        #validate operator
+        #This is to avoid confusion with the shell interpreting + as a command.
+        echo "Hint: If you're using "+", wrap it in quotes like: ./task1.sh 2 '+' 2"
         #check if the operator is valid
         if ! [[ "$2" =~ ^[\+\-\*\/]$ ]]; then
             echo "Error: Second argument is not a valid operator (+, -, *, /)."
             exit 1
-	fi
-
+        fi
+        # Handle division by zero
+        if [[ "$op" == "/" && "$num2" -eq 0 ]]; then
+            echo "Cannot divide by zero."
+            exit 1
+        fi
         #validate number
         if ! [[ "$3" =~ ^-?[0-9]+$ ]]; then
         echo "Error: Third argument is not a number."
         exit 1
         fi
-
         #If all validations pass, perform the calculation
         result=$(expr "$1" "$2" "$3")   
         echo "Answer = $result"
         #Display the result of the calculation
 }
-
 
 #Call the calculator function with the provided arguments
 calculator "$@"
