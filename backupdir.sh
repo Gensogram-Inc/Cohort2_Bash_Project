ef0b3b5aee2b9c13e1c162eeab9d2cc565f14d8f
Cloud User <ec2-user@ip-172-31-4-122.ca-central-1.compute.internal>
2025-04-11 01:22:30 +0000

    Updated code to rename with timestamp backupdir.sh

--- backupdir.sh
+++ backupdir.sh
@@ -2,3 +2,7 @@
 #Write a script that asks for a directory to back-up from the user, compresses it into a .tar.gz file and saves it the timestamp in the filename.
 read -p "Enter directory to back up" backupdir
 echo $backupdir
+timestamp=$(date +"%Y-%M-%D_%H-%M-%S")
+tar -cvf $backupdir.tar.gz .
+backupname="$backupdir.tar.gz_backupproject_$timestamp"
+echo "File backed-up $backupname"
