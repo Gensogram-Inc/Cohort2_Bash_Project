73e9c17314e179eeba695efe8132758c26f8fbcb
Cloud User <ec2-user@ip-172-31-4-122.ca-central-1.compute.internal>
2025-04-11 00:53:13 +0000

    Create first version backupdir.sh

---
+++ backupdir.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#Write a script that asks for a directory to back-up from the user, compresses it into a .tar.gz file and saves it the timestamp in the filename.
+read -p "Enter directory to back up" backupdir
+echo $backupdir
